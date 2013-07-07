5b6855d846c91f62f3c1cf76f4bc8f973c3f96d8
ransanjeev <ransanjeev@gmail.com>
2013-07-07 04:21:38 -0400

    Added method to read content from file

--- web.js
+++ web.js
@@ -3,10 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(geteDataFromFile);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function getDataFromFile(){
+    var fs= require('fs');
+    var data = fs.readFileSync('index.html');
+    return data;
+}
